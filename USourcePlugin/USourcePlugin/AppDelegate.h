dab12667b9fafc39da92ae76454fa2bfa4ef62a3
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-23 16:08:48 +0800

    添加插件工程

---
+++ USourcePlugin/USourcePlugin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  USourcePlugin
+//
+//  Created by suzhiqiu on 2019/8/23.
+//  Copyright © 2019 suzhiqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ USourcePlugin/USourcePlugin/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  USourcePlugin
+//
+//  Created by suzhiqiu on 2019/8/23.
+//  Copyright © 2019 suzhiqiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
