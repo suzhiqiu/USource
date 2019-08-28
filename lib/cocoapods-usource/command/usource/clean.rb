e1e5d2c29058077e3aa3556c40b583fbbde83d9e
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-28 09:49:14 +0800

    添加目录

---
+++ lib/cocoapods-usource/command/usource/clean.rb
@@ -0,0 +1,31 @@
+module Pod
+  class Command
+    class Usource < Command
+      class Clean < Usource
+        self.summary = '删除所有已经下载的源码'
+
+        def initialize(argv)
+          @update = argv.flag?('update')
+          @code  = argv.flag?('code')
+          @diff  = argv.flag?('diff')
+          super
+        end
+
+        def run
+         delete
+        end
+        
+        def delete()
+            UI.puts "开始删除...".red
+            # @path = ''
+            command = 'rm -rf  /Users/suzhiqiu/Downloads/q'
+            output = `#{command}`
+            UI.puts "#{output}"
+            UI.puts "结束删除...".red
+        end
+
+   
+      end
+    end
+  end
+end
