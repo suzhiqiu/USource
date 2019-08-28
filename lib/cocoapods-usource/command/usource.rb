004d49b60c13df1b76a70894fc9c76a486981383
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-28 15:16:37 +0800

    第一版OK了

--- lib/cocoapods-usource/command/usource.rb
+++ lib/cocoapods-usource/command/usource.rb
@@ -10,22 +10,12 @@ module Pod
       self.description = <<-DESC
         通过讲二进制对应源码放置再临时目录中,让二进制出现断点时可以跳转到对应的源码。
       DESC
-
-      self.arguments = 'NAME'
-
-      def initialize(argv)
-        @name = argv.shift_argument
-        super
-      end
-
-      def validate!
-        super
-        help!  unless @name
-      end
-
+      self.abstract_command = true
+      self.arguments = []
       def run
         UI.puts "Add your implementation for the cocoapods-usource plugin in #{__FILE__}"
       end
+
     end
   end
 end
