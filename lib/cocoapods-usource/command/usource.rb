43bf9b31bc7433eca8bcbd65a5da14173f4018be
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-28 10:10:21 +0800

    修改注释

--- lib/cocoapods-usource/command/usource.rb
+++ lib/cocoapods-usource/command/usource.rb
@@ -4,28 +4,11 @@ require 'cocoapods-usource/command/usource/clean'
 
 module Pod
   class Command
-    # This is an example of a cocoapods plugin adding a top-level subcommand
-    # to the 'pod' command.
-    #
-    # You can also create subcommands of existing or new commands. Say you
-    # wanted to add a subcommand to `list` to show newly deprecated pods,
-    # (e.g. `pod list deprecated`), there are a few things that would need
-    # to change.
-    #
-    # - move this file to `lib/pod/command/list/deprecated.rb` and update
-    #   the class to exist in the the Pod::Command::List namespace
-    # - change this class to extend from `List` instead of `Command`. This
-    #   tells the plugin system that it is a subcommand of `list`.
-    # - edit `lib/cocoapods_plugins.rb` to require this file
-    #
-    # @todo Create a PR to add your plugin to CocoaPods/cocoapods.org
-    #       in the `plugins.json` file, once your plugin is released.
-    #
     class Usource < Command
-      self.summary = 'Suzq Short description of cocoapods-usource.'
+      self.summary = '组件二进制化插件.'
 
       self.description = <<-DESC
-        Longer description of cocoapods-usource.
+        通过讲二进制对应源码放置再临时目录中,让二进制出现断点时可以跳转到对应的源码。
       DESC
 
       self.arguments = 'NAME'
@@ -37,7 +20,7 @@ module Pod
 
       def validate!
         super
-        help! 'A Pod name is required.' unless @name
+        help!  unless @name
       end
 
       def run
